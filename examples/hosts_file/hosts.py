da3fd7dcb6a080f66e1533c857bd55e36ce79eea
Łukasz Oleś <lukaszoles@gmail.com>
2016-02-01 14:09:43 +0100

    Nodes are note created in examples now
    To be able test examples in different envs nodes can not be created in examples.
    Now before running an example nodes should be created manually. For fuel-devops
    nodes will be created automaticly in run.sh script

--- examples/hosts_file/hosts.py
+++ examples/hosts_file/hosts.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python
-import click
-import sys
-import time
-
-from solar.core import signals
-from solar.core.resource import composer as cr
-from solar.dblayer.model import ModelMeta
+from solar.core.resource import resource as rs
 
 
 def run():
-    ModelMeta.remove_all()
 
-    resources = cr.create('nodes', 'templates/nodes', {'count': 2})
-
-    node1, node2 = [x for x in resources if x.name.startswith('node')]
-    hosts1, hosts2 = [x for x in resources
-                      if x.name.startswith('hosts_file')]
+    node1, node2 = rs.load_all(startswith='node')[:2]
+    hosts1, hosts2 = rs.load_all(startswith='hosts_file')[:2]
 
     node1.connect(hosts1, {
         'name': 'hosts:name',
