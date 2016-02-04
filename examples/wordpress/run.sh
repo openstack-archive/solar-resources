14329c10e9dae678b28702b59d71c6057c857dfd
Łukasz Oleś <lukaszoles@gmail.com>
2016-02-04 13:27:49 +0100

    Do not create nodes in test

--- examples/wordpress/run.sh
+++ examples/wordpress/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
 
-solar resource create nodes templates/nodes count=1
 solar repo import /vagrant/solar-resources/examples/wordpress/wp_repo
 solar resource create wp_docker wp_repo/docker
